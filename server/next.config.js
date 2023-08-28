5950d22d43a30c470687082e227232eb48fe7d0c
Hannes Güdelhöfer <hannes@guedelhoefer.de>
2023-08-28 13:20:36 +0200

    Fix build?

--- server/next.config.js
+++ server/next.config.js
@@ -9,7 +9,7 @@ if (
 ) {
 	process.env.LD_LIBRARY_PATH = `${
 		process.env.PWD
-	}/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+	}/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`
 }
 
 module.exports = nextConfig
