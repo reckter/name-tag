d4cb7b14fc98c24dce0d0e160a46a0321288b402
Hannes Güdelhöfer <hannes@guedelhoefer.de>
2024-08-05 20:26:18 +0200

    Dont get the pixel content

--- server/src/app/api/slides/listByUser/route.ts
+++ server/src/app/api/slides/listByUser/route.ts
@@ -1,13 +1,20 @@
-import {MONGO_DB, mongoClient} from "@/src/services/mongo"
+import { MONGO_DB, mongoClient } from "@/src/services/mongo"
 import { NextResponse } from "next/server"
 
 export async function GET(request: Request) {
-    const {searchParams} = new URL(request.url)
-    const user = searchParams.get("user")
+	const { searchParams } = new URL(request.url)
+	const user = searchParams.get("user")
 
-    const db = mongoClient.db(MONGO_DB)
-    const collection = db.collection("slides")
-    
-    const result = await collection.find({user: user}).toArray()
-    return NextResponse.json(result)
+	const db = mongoClient.db(MONGO_DB)
+	const collection = db.collection("slides")
+
+	const result = await collection.find({ user: user }, {
+		projection: {
+			user: 1,
+			id: 1,
+			name: 1,
+			chunkSize: 1
+		}
+	}).toArray()
+	return NextResponse.json(result)
 }
